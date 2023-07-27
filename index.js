31d295508a480a98a5b697bac6d01b6f068a57f8
ankit <ankit28sethi@gmail.com>
2023-07-27 17:04:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//dependencies required for the app
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+//render css files
+app.use(express.static("public"));
+
+//placeholders for added task
+var task = ["buy socks", "practise with nodejs"];
+//placeholders for removed task
+var complete = ["finish jquery"];
+
+//post route for adding new task 
+app.post("/addtask", function(req, res) {
+    var newTask = req.body.newtask;
+    //add the new task from the post route
+    task.push(newTask);
+    res.redirect("/");
+});
